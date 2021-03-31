95b145279a891002d690889b6ca48a8c19161964
Aravind R <aravindr597@gmail.com>
2021-03-31 17:37:54 +0530

    Fully Completed

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
 const spotifyApi = require("spotify-web-api-node");
 const cors = require("cors");
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 const spotifyAuth = new spotifyApi({
-  redirectUri : "http://localhost:3000",
-  clientId : "31794e88e1254626ad086c68c79cfb89",
-  clientSecret : "5909bab9bfc7460d8f0a0b46b46a927b",
+  redirectUri: process.env.REDIRECT_URI,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
 });
+
+
 
 app.post("/login", (req, res) => {
   const code = req.body.code;
@@ -49,6 +51,8 @@ app.post("/refresh", (req, res) => {
       console.log(err);
     });
 });
-app.listen(5000, () => {
+
+
+app.listen(PORT, () => {
   console.log("Server running");
 });
